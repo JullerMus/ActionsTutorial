8c78441fb6332534ee6f8e483acc9797a72e6434
JullerMus <juliusclykkegaard@gmail.com>
2025-02-08 09:40:24 +0100

    Initial workflow

--- src/main/java/org/example/actionstutorial/ActionsTutorialApplication.java
+++ src/main/java/org/example/actionstutorial/ActionsTutorialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActionsTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActionsTutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActionsTutorialApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/actionstutorial/ActionsTutorialApplicationTests.java
+++ src/test/java/org/example/actionstutorial/ActionsTutorialApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ActionsTutorialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
