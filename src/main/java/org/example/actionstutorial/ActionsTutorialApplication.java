d122ac840ae852f9e684745962c76dc6e4694f34
Julius Lykkegaard <juliuslykkegaard@Juliuss-MacBook-Air.local>
2025-02-08 09:32:44 +0100

    Initial commit

---
+++ src/main/java/org/example/actionstutorial/ActionsTutorialApplication.java
@@ -0,0 +1,13 @@
+package org.example.actionstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/actionstutorial/ActionsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.actionstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
